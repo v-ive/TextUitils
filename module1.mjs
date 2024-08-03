01d8a32dd18804957b61dcd94a1643ccb4eb2794
Vivek Kumar <96659150+v-ive@users.noreply.github.com>
2024-08-03 17:30:19 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import ui, {c,b, d}  from './module2.mjs';
+console.log(ui);
+console.log(b)
+console.log(c)
+console.log(d)
---
+++ module2.mjs
@@ -0,0 +1,11 @@
+const a = "satyam "
+const b = "baune ko bula"
+const c = "kon bauna"
+const d = "navin bauna"
+// const e = "Vaishali bhar aja "
+
+export default a;
+export {b};
+export {c};
+export {d};
+
